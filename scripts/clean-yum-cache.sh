bf3f2b629b0041d7aa751e6706e3660094c898af
root <root@orakvm.example.com>
2024-11-20 23:29:30 +0800

    Initial files pushing to the repository

---
+++ scripts/clean-yum-cache.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yum clean all >/dev/null 2>&1
---
+++ scripts/create-file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This file is modified to test
+touch /tmp/create-file.log
+echo "Ansible run @ $(date)" >> /tmp/create-file.log
